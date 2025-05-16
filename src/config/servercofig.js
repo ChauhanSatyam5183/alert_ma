817e8ee0dba5bdd8d7d5f2ffc30bb554765e9234
satyam chauhan <138180169+ChauhanSatyam5183@users.noreply.github.com>
2025-05-16 13:48:54 +0530

    Update servercofig.js

--- src/config/servercofig.js
+++ src/config/servercofig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports={
     PORT:process.env.PORT,
-    DB_URL:process.env.db_URL,
+    DB_URL:process.env.DB_URL,
     GEMINI_API_KEY : process.env.GEMINI_API_KEY,
     GEMINI_API_URL : process.env.GEMINI_API_URL           //export all env variable 
 }
